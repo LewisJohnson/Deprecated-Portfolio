6dd582cca76608c43a9065c8f601bb6020fc3804
[Lewis <lewisjohnsondev@gmail.com>
2016-08-26 00:44:51 +0100

    Added the browser zoom scale again.

--- js/lj-main.js
+++ js/lj-main.js
@@ -3,6 +3,10 @@
  */
 $(document).ready(function () {
 
+	var scale = 'scale(1)';
+	document.body.style.webkitTransform =  scale;    // Chrome, Opera, Safari
+ 	document.body.style.msTransform =   scale;       // IE 9
+ 	document.body.style.transform = scale;     // General
 
     $(".section").hide();
     $(".divider").hide();
