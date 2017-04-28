dc8f178399b649ba036588722761995ba0019c0f
Lewis Johnson <LewisJohnsonDev@gmail.com>
2017-04-28 22:30:21 +0100

    Update lj-main.js

--- js/lj-main.js
+++ js/lj-main.js
@@ -1,41 +1,22 @@
 /**
  * Created by Lewis Johnson on 24/07/2016.
  */
-$(document).ready(function () {
-
-    $('.intro-title').fadeIn(600);
-    $('.section').css('display', 'none');
-    var NameTicked = true;
-    var TimesTicked = 0;
-
-    name_tick();
-    function name_tick() {
-        if (NameTicked) {
-            NameTicked = false;
-            $(".intro-title").text("Lewis Johnson...");
-        } else {
-            NameTicked = true;
-            $('.intro-title').text("Lewis Johnson..|");
-        }
-        if (TimesTicked < 4) {
-            TimesTicked++;
-            setTimeout(name_tick, 500);
-        } else {
-            $('.intro-title').fadeOut(800);
-            $('body').toggleClass('light');
-            setTimeout(fadeLSecin, 800);
-        }
-    }
-    function fadeLSecin() {
-        $('#section1').fadeIn(400);
-        setTimeout(whatnext, 250);
-    }
-
-    function whatnext() {
-        $('#section2').fadeIn(400);
-        $('#section3').fadeIn(600);
-        $( ".sec-nav" ).each(function() {
-                $(this).hide();
-        });
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(textScramble, 500);
+    function textScramble() {
+        $('.intro-title').shuffle();
     }
 });
+
+String.prototype.shuffle = function () {
+    var a = this.split(""),
+        n = a.length;
+
+    for(var i = n - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var tmp = a[i];
+        a[i] = a[j];
+        a[j] = tmp;
+    }
+    return a.join("");
+}
