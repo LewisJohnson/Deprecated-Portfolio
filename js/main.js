ee86698b0b783779a9850656839d5f04cd74a4f2
Lewis Johnson <lewisjohnsondev@gmail.com>
2017-04-30 15:42:06 +0100

    Completly re-done portfolio... AGAIN

--- js/main.js
+++ js/main.js
@@ -2,21 +2,31 @@
  * Created by Lewis Johnson on 24/07/2016.
  */
 document.addEventListener("DOMContentLoaded", function(){
-    setTimeout(textScramble, 500);
-    function textScramble() {
-        document.getElementById("intro-title").shuffle();
-    }
+    // var swappedPattern;
+    //
+    // function swappedPattern() {
+    //     var overlay = document.getElementById("overlay");
+    //     if(swappedPattern){
+    //         overlay.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAANklEQVQYV2NkYGDY/P///zQGLIDx////koyMjLOwKQBLgjRhUwCXxKYARRJdAYYksgKskjAFAAaWKz9ed0kEAAAAAElFTkSuQmCC')";
+    //     } else {
+    //         overlay.style.backgroundImage = "url('data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAcAAAAHCAYAAADEUlfTAAAAPklEQVQYV2NkYGDY/P///zQGLIDx////koyMjM9BNLo8WBIkyMjIOAvdBLgkNgUoklAFcCswJJFNwCoJUwAACP4rPybpS5IAAAAASUVORK5CYII=')";
+    //     }
+    //     swappedPattern = !swappedPattern;
+    //     setTimeout(swappedPattern, 50);
+    // }
+    //
+    // swappedPattern();
 });
-
-String.prototype.shuffle = function () {
-    var a = this.split(""),
-        n = a.length;
-
-    for(var i = n - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var tmp = a[i];
-        a[i] = a[j];
-        a[j] = tmp;
-    }
-    return a.join("");
-};
+//
+// String.prototype.shuffle = function () {
+//     var a = this.split(""),
+//         n = a.length;
+//
+//     for(var i = n - 1; i > 0; i--) {
+//         var j = Math.floor(Math.random() * (i + 1));
+//         var tmp = a[i];
+//         a[i] = a[j];
+//         a[j] = tmp;
+//     }
+//     return a.join("");
+// };
