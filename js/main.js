f78b36d7f22f5a6b9e6e20a0a6ac638016288a4c
Lewis Johnson <lewisjohnsondev@gmail.com>
2017-08-13 16:05:06 +0100

    Added 2 new projects
    + Added source stats display
    + Added lang stats display
    - Removed snake game

--- js/main.js
+++ js/main.js
@@ -6,17 +6,6 @@ document.addEventListener("DOMContentLoaded", function(){
     var mainHeaderAccent = "rgba(0,210,255,0.2)";
     var lastHeaderInViewport;
 
-    function isInViewport(element) {
-        var rect = element.getBoundingClientRect();
-        var html = document.documentElement;
-        return (
-            rect.top >= 0 &&
-            rect.left >= 0 &&
-            rect.bottom <= (window.innerHeight || html.clientHeight) &&
-            rect.right <= (window.innerWidth || html.clientWidth)
-        );
-    }
-
     window.onscroll = function() {
 
         var sections = document.getElementsByClassName("section");
@@ -50,17 +39,37 @@ document.addEventListener("DOMContentLoaded", function(){
         }
     };
 
+    var lang_stats = document.getElementsByClassName('language-stats');
+    for (var i = 0; i < lang_stats.length; ++i) {
+        var list = lang_stats[i];
+
+        for (var j = 0; j < list.children.length; j++)
+        {
+            list.children[j].style.width = list.children[j].dataset.percentage;
+        }
+    }
+
+    var source_stats = document.getElementsByClassName('source-stats');
+    for (var i = 0; i < source_stats.length; ++i) {
+        var stats = source_stats[i];
+
+        for (var j = 0; j < stats.children.length; j++)
+        {
+            stats.children[j].style.width = (100 / stats.children.length) - 0.5 + "%";
+        }
+    }
+
+
 });
-//
-// String.prototype.shuffle = function () {
-//     var a = this.split(""),
-//         n = a.length;
-//
-//     for(var i = n - 1; i > 0; i--) {
-//         var j = Math.floor(Math.random() * (i + 1));
-//         var tmp = a[i];
-//         a[i] = a[j];
-//         a[j] = tmp;
-//     }
-//     return a.join("");
-// };
+
+
+function isInViewport(element) {
+    var rect = element.getBoundingClientRect();
+    var html = document.documentElement;
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || html.clientHeight) &&
+        rect.right <= (window.innerWidth || html.clientWidth)
+    );
+}
